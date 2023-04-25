0062644eff901bc61f25511013c8c993c0413aaa
MarshallLiu-Dev <paulomartins12358@gmail.com>
2023-04-24 21:43:14 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    // class Principal faz de conta que e veiculo
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // criar a herança 
+
+        Scanner ler = new Scanner(System.in);
+
+            veiculo obj = newVeiculo();
+			
+            
+    }
+}
---
+++ src/veiculo.java
@@ -0,0 +1,44 @@
+public class veiculo{
+        private int ano;
+        private String marca;
+        private String modelo;
+        private  String cor;
+
+   	public veiculo() {
+		this.ano = 0;
+		this.marca = "";
+		this.modelo = "";
+        this.cor = "";
+
+	}
+   
+  	public void setAno(int a) {
+        this.ano = a;
+	}
+	
+	public getAno() {
+		return ano;
+	}
+
+    public void setMarca(String) {
+		this.marca = "";
+	}
+	
+	public getMarca() {
+		return marca;
+	}
+    public void setModelo(String) {
+		this.modelo = "";
+	}
+	
+	public getModelo() {
+		return modelo;
+	}
+    public void setCor(String) {
+		this.cor = "";
+	}
+	
+	public getCar() {
+		return cor;
+	}
+}
